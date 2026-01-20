db01e23fe64d325c8bbc64dffed3667f33f6086d
takaken94 <takaken94@gmail.com>
2026-01-20 08:25:32 +0000

    feat: lambda 用 zip ファイル作成sh

---
+++ build_lambda_zip.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+ZIP_NAME=lambda_function.zip
+BUILD_DIR=build
+
+echo "== Clean build directory =="
+rm -rf ${BUILD_DIR}
+mkdir ${BUILD_DIR}
+
+echo "== Install dependencies =="
+pip install -r requirements.txt -t ${BUILD_DIR}
+
+echo "== Copy source files =="
+cp main.py ${BUILD_DIR}
+cp -r src ${BUILD_DIR}
+
+echo "== Remove unnecessary files =="
+find ${BUILD_DIR} -type d -name "__pycache__" -exec rm -rf {} +
+find ${BUILD_DIR} -type f -name "*.pyc" -delete
+rm -rf ${BUILD_DIR}/bin
+
+echo "== Create zip =="
+cd ${BUILD_DIR}
+zip -r ../${ZIP_NAME} .
+cd ..
+
+echo "== Done =="
+ls -lh ${ZIP_NAME}
