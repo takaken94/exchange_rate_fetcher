8f18ab78a1d7e5ce50e13727c80409cc3812f7ed
takaken94 <takaken94@gmail.com>
2026-02-21 15:32:27 +0900

    fix: migrate_json_xxx.py は除く

--- build_lambda_zip.sh
+++ build_lambda_zip.sh
@@ -5,15 +5,17 @@ ZIP_NAME=lambda_function.zip
 BUILD_DIR=build
 
 echo "== Clean build directory =="
+rm ${ZIP_NAME} 2>/dev/null || true
 rm -rf ${BUILD_DIR}
 mkdir ${BUILD_DIR}
+mkdir ${BUILD_DIR}/src
 
 echo "== Install dependencies =="
 pip install -r requirements.txt -t ${BUILD_DIR}
 
 echo "== Copy source files =="
 cp main.py ${BUILD_DIR}
-cp -r src ${BUILD_DIR}
+cp src/logging_config.py ${BUILD_DIR}/src
 
 echo "== Remove unnecessary files =="
 find ${BUILD_DIR} -type d -name "__pycache__" -exec rm -rf {} +
